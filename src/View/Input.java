7d0e8596e47c1b93f2332968898aa7e7ab23b591
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2023-11-24 12:01:15 +0100

    Input abstract class

---
+++ src/View/Input.java
@@ -0,0 +1,72 @@
+package View;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public abstract class Input {
+
+    private static Scanner scanner;
+
+    public Input() {
+        scanner = newScannerErrorHandling();
+    }
+
+    private Scanner newScannerErrorHandling () {
+        try {
+            return new Scanner(System.in);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            return null;
+        }
+    }
+
+    // get datatype input
+    public static int intInput(String message) {
+        System.out.println(message);
+        try {
+            return scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return 0;
+    }
+
+    public static double doubleInput(String message) {
+        System.out.println(message);
+        try {
+            return scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return 0;
+    }
+
+    public static String stringInput(String message) {
+        System.out.println(message);
+        try {
+            return scanner.next();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+
+    public static LocalDate dateInput(String message){
+        System.out.println(message);
+        return scanDate();
+    }
+
+    private static LocalDate scanDate() {
+
+        while (true) {
+            System.out.println("Enter date in format yyyy-mm-dd: ");
+            try {
+                return LocalDate.parse(scanner.next());
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+
+            }
+        }
+    }
+
+}
