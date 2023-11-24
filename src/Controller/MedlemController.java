d6ccc05c6d067f783cef479a7da429d3698ae5f1
MagnusMunkBraunstein <mabr0004@stud.kea.dk>
2023-11-24 13:28:28 +0100

    Constructor til oprettelse af medlem og get metoder. Kasper og Magnus

--- src/Controller/MedlemController.java
+++ src/Controller/MedlemController.java
@@ -1,4 +1,16 @@
 package Controller;
 
+import Model.*;
+
+import java.util.ArrayList;
+
 public class MedlemController {
+    public static ArrayList<Medlemskab> alleMedlemmer = new ArrayList<>();
+    public static ArrayList<AktivtMedlem> AktivMedlemsListe = new ArrayList<>();
+    public static ArrayList<PassivtMedlem> PassivtMedlemsListe = new ArrayList<>();
+    public static ArrayList<Motionist> MotionistListe = new ArrayList<>();
+    public static ArrayList<KonkurrenceSvoemmer> KonkurrenceSvoemmerListe = new ArrayList<>();
+    public static ArrayList<ArrayList> TopFemKonkurencSvoemmere;
+
+
 }
